bb9d804e19b4368579fb09b3776083ee6bb521d6
Mc-shelton <sheltonnito@gmail.com>
2025-04-28 05:51:44 +0300

    shared

--- api/quarterlies/index.js
+++ api/quarterlies/index.js
@@ -1,6 +1,7 @@
 // import type { VercelRequest, VercelResponse } from '@vercel/node';
 import axios from 'axios';
 import {picker} from '../../bff/adapters/picker.js';
+import corsMiddleware from '../cors.js';
 const errorEnums = {
     SERVER: 'internal server error. try again later',
     FIELDS: 'you must provid all the fields',
@@ -10,18 +11,6 @@ const errorEnums = {
 const url = 'https://sabbath-school.adventech.io/api/v2/'
 
 
-function corsMiddleware(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return true;
-    }
-
-    return false;
-}
 const dbUrl = "mysql://tech_support:wArkB_3nch@be.opencapital.com:3306/oca_fun"
 const dbT = process.env.DATABASE_URL
 
